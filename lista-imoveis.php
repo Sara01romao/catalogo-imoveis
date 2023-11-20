066d1ba68cfac0cbe2909fbfefbb0aceab87c3bf
Sara Romao <sara-romao@live.com>
2023-11-19 23:23:00 -0300

    feat: lista de imóveis

---
+++ lista-imoveis.php
@@ -0,0 +1,83 @@
+<?php
+
+$imoveis = [
+    [
+        'id' => 1,
+        'title' => 'Casa de Campo',
+        'description' => 'Desfrute da paz e tranquilidade no campo nesta encantadora casa rodeada por natureza exuberante. Com uma atmosfera acolhedora e espaços amplos, esta casa oferece o refúgio ideal para escapar da agitação da cidade. Explore trilhas serenas, respire ar puro e aproveite o charme simples da vida no campo.',
+        'Cidade' => 'São Paulo',
+        'Bairro' => 'Centro',
+        'Quartos' => 3,
+        'Banheiros' => 2,
+        'Garagem' => 2,
+        'proximo' => ['Escola', 'Farmacia', 'Super Mercado'],
+        'img' => 'prog-1.jpg'
+    ],
+
+    [
+        'id' => 2,
+        'title' => 'Apartamento no Centro',
+        'description' => 'Localizado no coração da cidade, este apartamento oferece conveniência e estilo de vida urbano. Com acesso fácil a shoppings, restaurantes e centros culturais, desfrute do melhor que São Paulo tem a oferecer.',
+        'Cidade' => 'São Paulo',
+        'Bairro' => 'Centro',
+        'Quartos' => 2,
+        'Banheiros' => 1,
+        'Garagem' => 1,
+        'proximo' => ['Shopping', 'Restaurante', 'Teatro'],
+        'img' => 'centro-1.jpg'
+    ],
+
+
+    [
+        'id' => 3,
+        'title' => 'Casa no Morumbi',
+        'description' => 'Esta espaçosa casa no Morumbi oferece conforto e privacidade em um dos bairros mais prestigiados de São Paulo. Com jardim exuberante e ambientes amplos, é o lar perfeito para quem busca tranquilidade sem abrir mão da sofisticação.',
+        'Cidade' => 'São Paulo',
+        'Bairro' => 'Morumbi',
+        'Quartos' => 4,
+        'Banheiros' => 3,
+        'Garagem' => 2,
+        'proximo' => ['Parque', 'Supermercado', 'Escola Internacional'],
+        'img' => 'morumbi-1.jpg'
+    ],
+    
+
+    
+  
+
+    [
+        'id' => 4,
+        'title' => 'Chácara com Piscina',
+        'description' => 'Desfrute de momentos relaxantes nesta chácara espaçosa, completa com uma grande piscina e áreas verdes exuberantes.',
+        'Cidade' => 'São Paulo',
+        'Bairro' => 'Zona Rural',
+        'Quartos' => 5,
+        'Banheiros' => 4,
+        'Garagem' => 3,
+        'proximo' => ['Supermercado', 'Trilhas', 'Pesqueiro'],
+        'img' => 'chacara-1.jpg'
+    ],
+
+
+    [
+        'id' => 5,
+        'title' => 'Loft na Vila Olímpia',
+        'description' => 'Desfrute da vida moderna nesta elegante loft na Vila Olímpia. Com design contemporâneo e acesso fácil a centros comerciais e de entretenimento, este é o lugar ideal para quem busca a combinação perfeita entre estilo e praticidade.',
+        'Cidade' => 'São Paulo',
+        'Bairro' => 'Vila Olímpia',
+        'Quartos' => 1,
+        'Banheiros' => 1,
+        'Garagem' => 1,
+        'proximo' => ['Shopping', 'Bares', 'Centro Empresarial'],
+        'img' => 'vila-olimpia-1.jpg'
+    ]
+    
+
+    
+    
+    
+  ];
+
+
+
+?>
